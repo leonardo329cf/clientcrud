59f8a1ea9d225355e2e0a7ac6f391cd2a8307507
leonardo329cf <leonardo329cf@gmail.com>
2021-08-14 00:22:08 -0300

    Client delete; DatabaseException

--- src/main/java/com/ferreira/clientcrud/resources/ClientResource.java
+++ src/main/java/com/ferreira/clientcrud/resources/ClientResource.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,11 @@ public class ClientResource {
 	public ResponseEntity<ClientDTO> uptade(@PathVariable Long id, @RequestBody ClientDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/ferreira/clientcrud/services/ClientService.java
+++ src/main/java/com/ferreira/clientcrud/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.ferreira.clientcrud.dto.ClientDTO;
 import com.ferreira.clientcrud.entities.Client;
 import com.ferreira.clientcrud.repositories.ClientRepository;
+import com.ferreira.clientcrud.services.exceptions.DatabaseException;
 import com.ferreira.clientcrud.services.exceptions.ResourceNotFoundException;
 
 @Repository
@@ -63,5 +66,18 @@ public class ClientService {
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
 		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 }
